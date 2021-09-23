0030ec22a0f8d11ee7b00f39b77417c506c0dc9f
luigi2001 <luigitanga@virgilio.it>
2021-09-23 18:24:47 +0200

    first commit

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <header>
+        <div class="logo">
+            <h1>Boolpress blog</h1>
+            <ul>
+                <li><router-link :to="{ name: 'home' }">Home</router-link></li>
+                <li><router-link :to="{ name: 'contact' }">Contatti</router-link></li>
+                <li><router-link :to="{ name: 'about' }">Chi siamo</router-link></li>
+            </ul>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+header{
+    height: 200px;
+    padding: 20px;
+    text-align: center;
+    .logo{
+        padding: 10px;
+        background-color: yellow;
+        border: 5px solid blue;
+        border-radius: 15px;
+        ul{
+        list-style: none;
+        li{
+            display: inline-block;
+            margin-right: 20px;
+        }
+    }
+    }
+}
+</style>
---
+++ resources/js/front.js
@@ -0,0 +1,13 @@
+require ('./bootstrap');
+
+window.Vue = require('vue');
+window.axios = require('axios');
+
+import App from './views/App';
+import router from './router';
+
+const app = new Vue({
+    el: '#root',
+    render: h => h(App),
+    router
+});
